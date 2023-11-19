dfdfcabab7bbfba45cc42ade1961f5b3462753d3
Willians Patiño <ppwj@yahoo.com>
2023-11-19 19:54:00 -0400

    add more Perl components to install

--- 00__update_components.sh
+++ 00__update_components.sh
@@ -2,5 +2,29 @@
 
 sudo dnf update && sudo dnf install -y imapsync && sudo dnf install -y ccze
 
+  yum install perl-App-cpanminus \
+ perl-Dist-CheckConflicts \
+ perl-HTML-Parser \
+ perl-libwww-perl \
+ perl-Module-Implementation \
+ perl-Module-ScanDeps \
+ perl-Package-Stash \
+ perl-Package-Stash-XS \
+ perl-PAR-Packer \
+ perl-Regexp-Common \
+ perl-Sys-MemInfo \
+ perl-Test-Fatal \
+ perl-Test-Mock-Guard \
+ perl-Test-Requires \
+ perl-Test-Deep \
+ perl-File-Tail \
+ perl-Unicode-String \
+ perl-Test-NoWarnings \
+ perl-Test-Simple \
+ perl-Test-Warn \
+ perl-Sub-Uplevel \
+ perl-Proc-ProcessTable \
+ ca-certificates
+
 
 
--- 01__update_perl-comp.sh
+++ 01__update_perl-comp.sh
@@ -2,4 +2,6 @@
 
 # Install Perl modules using cpan   (19.11.2023, 5:32am)
 sudo cpan
-install File::NFSlock
+install File::NFSlock 
+install IO::Socket::IP
+
