1d38b50c3265e1ec6a1cfc0e3e09560e62254278
Willians Patiño <ppwj@yahoo.com>
2023-11-19 23:14:14 -0400

    Firewall

--- 04__abrir-puertos.sh
+++ 04__abrir-puertos.sh
@@ -5,5 +5,5 @@
  sudo firewall-cmd --zone=public --add-service=imap --permanent
  sudo firewall-cmd --zone=public --add-service=imaps --permanent
 
- firewalll-cmd --reload
+ firewall-cmd --reload
  firewall-cmd --zone=public --list-all
