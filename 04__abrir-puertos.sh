24b366e3ee9767de944918d7ff164c46d6018ce9
Willians Patiño <ppwj@yahoo.com>
2023-11-19 23:06:59 -0400

    Puerto esenciales para migracion

--- 04__abrir-puertos.sh
+++ 04__abrir-puertos.sh
@@ -2,5 +2,5 @@
 
 # permitir los puertos esenciales para la migración
 
- sudo firewall-cmd --zone=public --add-service=143 --permanent
- sudo firewall-cmd --zone=public --add-service=993 --permanent
+ sudo firewall-cmd --zone=public --add-service=imap --permanent
+ 
