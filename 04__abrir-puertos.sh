8bb1fcf1c66d152b96586322f69254ed04bcc3bb
Willians Patiño <ppwj@yahoo.com>
2023-11-19 23:13:42 -0400

    Firewall

--- 04__abrir-puertos.sh
+++ 04__abrir-puertos.sh
@@ -3,4 +3,7 @@
 # permitir los puertos esenciales para la migración
 
  sudo firewall-cmd --zone=public --add-service=imap --permanent
+ sudo firewall-cmd --zone=public --add-service=imaps --permanent
+
+ firewalll-cmd --reload
  firewall-cmd --zone=public --list-all
